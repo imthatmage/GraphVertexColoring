f29fc81966dbb7d87ce78a25d2c9313e3bfcb5c4
Liram <nurm.almir@gmail.com>
2021-04-18 17:42:27 +0300

    Added graph implementation

---
+++ iterative_coloring/graph.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include "vertex.hpp"
+#include <list>
+#include <algorithm>
+#include <vector>
+
+class graph final
+{
+public:
+    graph(size_t size) : size_(size), colors(size) {}
+
+    void sort()
+    {
+        working.sort();
+    }
+    size_t size() { return size_; }
+
+    std::list<vertex>::iterator begin()
+    {
+        return working.begin();
+    }
+
+    std::list<vertex>::iterator end()
+    {
+        return working.end();
+    }
+
+    void assign(const std::vector<vertex>& hgraph)
+    {
+        for (auto& vert : hgraph)
+            working.push_back(vert);
+        adjacent = hgraph;
+    }
+
+    auto erase(std::list<vertex>::iterator& itera)
+    {
+        return working.erase(itera);
+    }
+
+    void set_color(size_t id, size_t color)
+    {
+        color_vertexes[color - 1].push_back(id);
+        colors[id] = color;
+    }
+
+    bool is_empty()
+    {
+        return working.size() == 0;
+    }
+
+    const std::vector<size_t>& get_colors()
+    {
+        return colors;
+    }
+
+    void initialize_color_vertex()
+    {
+        color_vertexes.push_back(std::vector<size_t>());
+    }
+
+    //returns vertexes colored with i
+    std::vector<size_t> get_vertexes(size_t i)
+    {
+        return color_vertexes[i];
+    }
+    vertex& get_vertex(size_t i )
+    {
+        return adjacent[i];
+    }
+    size_t chromatic_number;
+private:
+    size_t size_ = 0;
+    //working vertex storage
+    std::list<vertex> working;
+    //colors[i] color of i vertex
+    std::vector<size_t> colors;
+    //color_vertexes[i] vector with vertexes colored with i
+    std::vector<std::vector<size_t>> color_vertexes;
+    std::vector<vertex> adjacent;
+};
