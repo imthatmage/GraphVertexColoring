b64d1c779f07f126d7c3f3684289bfd1a8a8ffeb
Liram <nurm.almir@gmail.com>
2021-04-18 17:43:17 +0300

    Added one file tester

---
+++ iterative_coloring/test_with_file.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include "iterative_algorithm.hpp"
+
+int main()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    std::string inputfile = "C:/Users/nurma/Documents/repos/graph_coloring/data/gc_1000_9";
+    std::string outputfile = "tmp_result.dat";
+
+    std::ifstream inputData(inputfile);
+
+    size_t edges, vertexes;
+
+    if (!inputData.is_open())
+    {
+        std::cout << "Can't read this file!";
+        return -1;
+    }
+    inputData >> vertexes >> edges;
+    std::cout << edges << ' ' << vertexes << std::endl;
+    graph Graph(vertexes);
+    //assign graph
+    std::vector<vertex> assign_graph;
+    for (size_t i = 0; i < vertexes; ++i)
+    {
+        assign_graph.push_back(vertex(i));
+    }
+
+    for (size_t i = 0; i < edges; ++i)
+    {
+        size_t a, b;
+        inputData >> a >> b;
+        assign_graph[a].push_back(b);
+        assign_graph[b].push_back(a);
+    }
+    Graph.assign(assign_graph);
+    inputData.close();
+    std::ofstream outputData(outputfile);
+
+    Graph = iterative_algorithm::algorithm(Graph);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto curr_result = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();  
+    std::cout << std::endl;
+
+    //std::for_each(colors.begin(), colors.end(), [](auto elem) { static size_t i = 0; std::cout << i++ << ' ' << elem << std::endl; });
+
+    //checking correctness
+    std::cout << "Colors: " << Graph.chromatic_number << std::endl;
+    Graph = iterative_algorithm::improve(Graph, 50);
+    auto colors = Graph.get_colors();
+    //std::for_each(colors.begin(), colors.end(), [](auto elem) { static size_t i = 0; std::cout << i++ << ' ' << elem << std::endl; });
+    for (size_t i = 0; i < vertexes; ++i)
+    {
+        for (auto neigh : Graph.get_vertex(i))
+            if (colors[neigh] == colors[i])
+                std::cout << "Atas";
+    }
+
+    std::cout << "Colors: " << Graph.chromatic_number << std::endl;
+    std::cout << "Time: " << curr_result << " 10*-3 seconds" << std::endl;
+}
